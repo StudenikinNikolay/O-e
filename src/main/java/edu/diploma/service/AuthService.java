e45ab1d45f4fc40b1cf3e2cbad65bafd0838bc9c
StudenikinNikolay <165457990+StudenikinNikolay@users.noreply.github.com>
2025-03-26 21:05:46 +0300

    Update AuthService.java

--- src/main/java/edu/diploma/service/AuthService.java
+++ src/main/java/edu/diploma/service/AuthService.java
@@ -2,11 +2,14 @@ package edu.diploma.service;
 
 import edu.diploma.auth.JwtHelper;
 import edu.diploma.auth.UserCreds;
+import edu.diploma.controller.AuthController;
 import edu.diploma.model.Login;
 import edu.diploma.model.LoginErrors;
 import edu.diploma.model.User;
 import edu.diploma.repository.UserRepository;
 import io.vavr.control.Either;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
@@ -23,6 +26,7 @@ import java.util.Optional;
 @Service
 public class AuthService {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthService.class);
     private final JwtHelper jwtHelper;
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
@@ -44,14 +48,17 @@ public class AuthService {
         LoginErrors errors = new LoginErrors();
 
         if (Objects.isNull(creds)) {
+            log.info(String.format("Invalid credentials: null"));
             return Either.left(errors.addEmailMsg("Неправильные учетные данные"));
         }
 
         if (Objects.isNull(creds.getLogin()) || creds.getLogin().trim().isEmpty()) {
+            log.info(String.format("User Email not provided"));
             return Either.left(errors.addEmailMsg("Необходимо ввести почту"));
         }
 
         if (Objects.isNull(creds.getPassword()) || creds.getPassword().trim().isEmpty()) {
+            log.info(String.format("User Password not provided"));
             return Either.left(errors.addPasswordMsg("Необходимо ввести пароль"));
         }
 
@@ -66,12 +73,15 @@ public class AuthService {
             if (passwordEncoder.matches(creds.getPassword(), user.get().getPassword())) {
                 user.get().setToken(jwtHelper.createToken(Map.ofEntries(), user.get().getUsername()));
                 userRepository.save(user.get());
+                log.info(String.format("User authenticated: %s", creds.getLogin()));
                 return Either.right(new Login(user.get().getToken()));
             }
 
+            log.info(String.format("Incorrect password"));
             return Either.left(errors.addPasswordMsg("Неправильно указан пароль"));
         }
 
+        log.info(String.format("Incorrect email"));
         return Either.left(errors.addEmailMsg("Неправильно указана почта"));
 
     }
