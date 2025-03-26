35faad25170e4160556b8f9397ad280394e356fd
StudenikinNikolay <165457990+StudenikinNikolay@users.noreply.github.com>
2025-03-26 21:04:12 +0300

    Update AuthController.java

--- src/main/java/edu/diploma/controller/AuthController.java
+++ src/main/java/edu/diploma/controller/AuthController.java
@@ -5,6 +5,9 @@ import edu.diploma.model.Login;
 import edu.diploma.auth.UserCreds;
 import edu.diploma.service.AuthService;
 import io.vavr.control.Either;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -12,9 +15,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@Slf4j
 @RestController
 public class AuthController {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
     private final AuthService authService;
 
     public AuthController(
@@ -29,9 +34,13 @@ public class AuthController {
 
         Either<LoginErrors, Login> login = authService.login(creds);
 
-        return login.isRight()
-                ? ResponseEntity.ok().body(login.get())
-                : ResponseEntity.status(HttpStatus.BAD_REQUEST).body(login.getLeft());
+        if (login.isRight()) {
+            log.info(String.format("User logged in: %s", creds.getLogin()));
+            return ResponseEntity.ok().body(login.get());
+        }
+
+        log.info(String.format("Login attempt failed: %s",login.getLeft()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(login.getLeft());
 
     }
 
